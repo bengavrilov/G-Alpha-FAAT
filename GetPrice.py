0da491871d6f821883b237ec182b341bbc66b9e5
Daniel Genkin <genkindan13@gmail.com>
2021-05-10 00:35:47 -0400

    get price of stock

--- GetPrice.py
+++ GetPrice.py
@@ -2,41 +2,23 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Obtain MSFT 
 msft = yf.Ticker("MSFT")
 print(msft)
-"""
-returns
-<yfinance.Ticker object at 0x1a1715e898>
-"""
-
-# get stock info
-a = msft.institutional_holders
-
-"""
-returns:
-{
- 'quoteType': 'EQUITY',
- 'quoteSourceName': 'Nasdaq Real Time Price',
- 'currency': 'USD',
- 'shortName': 'Microsoft Corporation',
- 'exchangeTimezoneName': 'America/New_York',
-  ...
- 'symbol': 'MSFT'
-}
-"""
 
 # get historical market data, here max is 5 years.
 stock_prices = msft.history(period="max")
 print(stock_prices)
 
-
-
+# Get the close price for every day 
 close_prices = stock_prices.iloc[:,3].values
-dates = np.arange(len(close_prices))
+dates = np.arange(len(close_prices)) # make an array for the days to plot
 
+# moving average calculation
 MA = np.convolve(close_prices, np.ones(10), 'valid') / 10
 #print(MA)
 
+# Plot 
 plt.plot(dates,close_prices)
 plt.plot(dates[8:-1],MA)
 plt.title('Microsoft Price')
